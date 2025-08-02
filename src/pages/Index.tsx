59f3cf2312bbbe4e06682541e5e0043a43e683f9
jurisgrisins <juris.grisins@gmail.com>
2025-08-02 22:56:32 +0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -14,7 +14,7 @@ const Index = () => {
           <div className="mb-8">
             <p className="text-primary text-lg font-medium mb-4">Sveiks!</p>
             <h1 className="text-4xl md:text-5xl font-medium text-foreground leading-loose mb-8">
-              Mēs piedāvājam apmācības kā veiksmīgi uzsākt ieguldījumus kriptovalūtās
+              Mēs piedāvājam profesionālas apmācības kā veiksmīgi uzsākt ieguldījumus kriptovalūtās
             </h1>
           </div>
           
