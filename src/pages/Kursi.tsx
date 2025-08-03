0bfbf2ba4998c9ba3a949e6fbeb870a386c44306
jurisgrisins <juris.grisins@gmail.com>
2025-08-03 10:00:44 +0300

    Update Kursi.tsx

--- src/pages/Kursi.tsx
+++ src/pages/Kursi.tsx
@@ -15,13 +15,17 @@ const navItems = [
 const Kursi = () => {
   return (
     <BjorkLayout>
-      <div className="flex gap-8">
-        {/* Sidebar Navigation */}
-        <div className="w-64 flex-shrink-0">
-          <div className="sticky top-8">
-            <SidebarNav items={navItems} className="mb-8" />
-          </div>
-        </div>
+   <div>
+  {/* Sidebar Navigation */}
+  <div className="mb-8">
+    <SidebarNav items={navItems} />
+  </div>
+  {/* Main Content */}
+  <div className="mx-auto max-w-3xl px-8 py-16">
+    {/* ... */}
+  </div>
+</div>
+
 
         {/* Main Content */}
         <div className="flex-1 mx-auto px-8 py-16">
