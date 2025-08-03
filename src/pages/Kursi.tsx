eba919139db972fa21fcabb3b9eb74a6af35f7c5
jurisgrisins <juris.grisins@gmail.com>
2025-08-03 11:22:20 +0300

    Update Kursi.tsx

--- src/pages/Kursi.tsx
+++ src/pages/Kursi.tsx
@@ -23,7 +23,7 @@ const Kursi = () => {
     <BjorkLayout>
       <div className="flex gap-8">
         {/* Sidebar Navigation */}
-         <div className="mb-8 mt-16">
+         <div className="mb-8">
     <SidebarNav items={navItems} />
   </div>
 
